82d47a2b629d23b0eeb0fb2e9653138ac5780167
Rodrigo Gonçalves <rodrigo.goncalvesss@outlook.com>
2021-04-14 16:14:56 -0300

    Aplicações da criação do projeto

---
+++ src/main/java/com/rodrigo/aulaparticular/AulaparticularApplication.java
@@ -0,0 +1,16 @@
+package com.rodrigo.aulaparticular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulaparticularApplication  {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AulaparticularApplication.class, args);
+    }
+
+
+
+    }
+
